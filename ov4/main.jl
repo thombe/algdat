4352b92b1c774c9034999a5df7797b61be307044
thombe <thobern@stud.ntnu.no>
2018-09-14 10:03:47 +0200

    q2 done ish

--- ov4/main.jl
+++ ov4/main.jl
@@ -19,5 +19,31 @@ function counting_sort_letters(A, position::Int)
     return B
 end
 
-A = ["ccc", "cba", "ca", "ab", "abc"]
-counting_sort_letters(A , 2)
+function counting_sort_length(A)
+    k = length(A[1])
+    for i = 2:length(A)
+        if length(A[i]) > k
+            k = length(A[i])
+        end
+    end
+
+    C = zeros(Int , k)
+    B = deepcopy(A)
+
+    for j = 1:length(A)
+        C[length(A[j])] += 1
+    end
+
+    for i = 2:k
+        C[i] += C[i-1]
+    end
+
+    for j = length(A):-1:1
+        B[C[length(A[j])]] = A[j]
+        C[length(A[j])] -= 1
+    end
+    return B
+end
+
+A = ["bbbb" , "aa" , "aaaa" , "ccc"]
+counting_sort_length(A)
