03a40173345ba62cce07d5bdbc46a54c5a532526
thombe <thobern@stud.ntnu.no>
2018-09-14 09:30:17 +0200

    q1 done

--- ov4/main.jl
+++ ov4/main.jl
@@ -1,21 +1,23 @@
 function counting_sort_letters(A, position::Int)
     k = 26
     C = zeros(Int , k)
+    B = deepcopy(A)
 
     for j = 1:length(A)
-        C[A[j]] += 1
+        C[Int(A[j][position]-96)] += 1
     end
-    println(A)
 
-    for i = 1:k
+    for i = 2:k
         C[i] += C[i-1]
     end
 
-    for j = length(A):1
-        B[C[A[j]]] = A[j]
-        C[A[j]] -= 1
+    for j = length(A):-1:1
+        B[C[Int(A[j][position]-96)]] = A[j]
+        C[Int(A[j][position]-96)] -= 1
     end
 
-
-
+    return B
 end
+
+A = ["ccc", "cba", "ca", "ab", "abc"]
+counting_sort_letters(A , 2)
