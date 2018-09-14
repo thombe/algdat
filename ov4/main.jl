f5eb5fa3fdf94293f49d32acb35538f12843233c
thombe <thobern@stud.ntnu.no>
2018-09-14 14:29:42 +0200

    q3 done

--- ov4/main.jl
+++ ov4/main.jl
@@ -1,9 +1,13 @@
 function counting_sort_letters(A, position::Int)
+
     k = 26
     C = zeros(Int , k)
     B = deepcopy(A)
 
     for j = 1:length(A)
+    #    if position > length(A[j])
+    #        break
+    #    end
         C[Int(A[j][position]-96)] += 1
     end
 
@@ -33,11 +37,11 @@ function counting_sort_length(A)
     for j = 1:(length(A))
         C[length(A[j])+1] += 1
     end
-    println(C)
+    #println(C)
     for i = 2:k+1
         C[i] = C[i] +  C[i-1]
     end
-    println(C)
+    #println(C)
 
     for j = length(A):-1:1
         B[C[length(A[j])+1]] = A[j]
@@ -46,6 +50,34 @@ function counting_sort_length(A)
     return B
 end
 
-A = ["bbbb", "aa", "aaaa", "ccc"]
-println(A)
-println(counting_sort_length(A))
+function flexradix(A , max_length)
+    B = counting_sort_length(A)
+    println(B)
+    #k = length(B)
+    for i = max_length:-1:1
+        k = 1
+        for j = length(B):-1:1
+            if length(B[j]) >= i
+                continue
+            else
+                println(B[j] , " wasnt longer or equal than " , i , " and thus k = " , j + 1 , '\n')
+                k = j + 1
+                break
+            end
+        end
+#        println(" k is now: " , k)
+        println(B[k:end] , " \nis about to be sortet by the pos " , i)
+        B[k:end] = counting_sort_letters(B[k:end] , i)
+#        println("Round " , max_length-i+1 , " is done")
+        println(B)
+        println('\n')
+    end
+    return B
+end
+
+A = ["kobra", "aggie", "agg", "kort", "hyblen"]
+B = ["kobra", "alge", "agg", "kort", "hyblen", "i"]
+println(flexradix(B, 6))
+#B = counting_sort_length(A)
+#B[1:2] = counting_sort_letters(B[1:2] , 2)
+#println(B)
