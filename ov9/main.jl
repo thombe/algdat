b34c28ff0a8ff04d338ec109b015c464f4594239
thobern <thobern@stud.ntnu.no>
2018-10-22 19:00:53 +0200

    done

--- ov9/main.jl
+++ ov9/main.jl
@@ -46,37 +46,69 @@ end
 
 function findclusters(E::Vector{Tuple{Int, Int, Int}}, n::Int, k::Int)
     R = []
+    for i = 1:k
+        push!(R,[])
+    end
     nodes = []
-    picked = []
+    edges = []
+
     for i = 1:n
         node = DisjointSetNode()
         node.p = node
         node.rank = 0
         push!(nodes,node)
-        push!(picked,[])
     end
     sort!(E)
     trees = n
-    println("picked is now " ,picked)
+
     for i = 1:length(E)
+        if trees == k
+            break
+        end
         u = E[i][2]
         v = E[i][3]
         if findset(nodes[u]) != findset(nodes[v])
             unionn!(nodes[u], nodes[v])
-            #push!(R , [u,v])
-            push!(picked[u],v)
+            push!(edges,E[i])
             trees -= 1
-            if trees == k
-                break
-            end
         end
     end
-    for u in picked
-        if !isempty(u)
-            push!(R,u)
-        end
+
+    clusters = Dict{DisjointSetNode, Vector{Int}}()
+    for i = 1:n
+        node = findset(nodes[i])
+        group = get!(clusters, node, Vector{Int}())
+        push!(group, i)
     end
-    return R
+    return collect(Vector{Int} , values(clusters))
 end
-E = [(5,1,2),(3,1,3), (4,3,4), (1,2,3), (2,4,1), (6,1,5) , (7,2,4)]
-println(findclusters(E,5,1))
+
+
+E = [(5,1,2),(3,1,3), (4,3,4), (1,2,3), (2,1,4), (6,1,5) , (7,2,4)]
+println(findclusters(E,5,2))
+
+
+function findanimalgroups(animals::Vector{Tuple{String, String}}, k::Int64)
+    E = Vector{Tuple{Int, Int, Int}}()
+    for i = 1:length(animals)
+        for j = 1:i-1
+            push!(E,(hammingdistance(animals[i][2],animals[j][2]), i , j,))
+        end
+    end
+    R = findclusters(E,length(animals) , k)
+    println(R)
+    F = []
+    for i = 1:k
+        push!(F,[])
+    end
+    println(F)
+    for i = 1:length(R)
+        for j = 1:length(R[i])
+            push!(F[i],animals[R[i][j]][1])
+        end
+    end
+    return F
+
+end
+
+println(findanimalgroups([("Ugle", "CGGCACGT"), ("Elg", "ATTTGACA"), ("Hjort", "AATAGGCC")] , 2))
