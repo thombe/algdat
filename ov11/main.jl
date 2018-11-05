adbd72fd4953a062e9a04a765462a1d13333f393
thobern <thobern@stud.ntnu.no>
2018-11-05 19:29:18 +0100

    q4 ov11

--- ov11/main.jl
+++ ov11/main.jl
@@ -1,25 +1,62 @@
 function floyd_warshall(adjacency_matrix, nodes , f, g)
     n , m = size(adjacency_matrix)
     D = adjacency_matrix
-    Pi = adjacency_matrix
-    for row = 1:n
-        for col = 1:m
-            if Pi[row,col] > 0
-                Pi[row,col] = row
-            end
-        end
-    end
 
     for k = 1:n
         for i = 1:n
             for j = 1:n
-                if f(D[i,j],g(D[i,k],D[k,j]))
-                    D[i,j] = g(D[i,k],D[k,j])
-                    Pi[i,j] = Pi[k,j]
-                end
+                D[i,j] = f(D[i,j],g(D[i,k],D[k,j]))
             end
         end
     end
-
-    return D, Pi
+    return D
 end
+
+function transitive_closure(adjacency_matrix, nodes)
+    R = floyd_warshall(adjacency_matrix, nodes , min , +)
+    println(R)
+    m, n = size(adjacency_matrix)
+    for i = 1:m
+        for j = 1:n
+            if R[i,j] > 999999
+                R[i,j] = 0
+            else
+                R[i,j] = 1
+            end
+
+        end
+    end
+
+    return R
+end
+
+function create_preference_matrix(ballots, voters, candidates)
+    P = zeros(candidates,candidates)
+    for i = 1:voters
+        for j = 1:candidates
+            c = ballots[i][j]
+            for char in ballots[j+1:end]
+                P[c-'A'+1,char-'A'+1] += 1
+            end
+        end
+    end
+    return P
+end
+
+function find_strongest_paths(preference_matrix, candidates)
+     m , n = size(preference_matrix)
+     P = preference_matrix
+     for i = 1:m
+         for j = 1:n
+             if P[i,j] < P[j,i]
+                 P[i,j] = 0
+             else
+                 P[j,i] = 0
+             end
+         end
+     end
+     P = floyd_warshall(P , candidates , max , +)
+     return P
+ end
+
+ println(find_strongest_paths([0 11 20 14; 19 0 9 12; 10 21 0 17; 16 18 13 0], 4))
